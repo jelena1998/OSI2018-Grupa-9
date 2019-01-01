fa5adf3b5867b2907e9b0b0b33264de62ed6cf21
jelena1998 <jelenazaric1998@gmail.com>
2019-01-02 00:12:08 +0100

    Kreiranje kljuca - dodano upisivanje rednog broja kljuca u dat

--- Kljuc/kljuc.cpp
+++ Kljuc/kljuc.cpp
@@ -5,11 +5,10 @@
 #include<time.h>
 void pisiKljuc();
 int provjeri(char*, FILE*);
-
+int index(FILE* f);
 
 int main()
 {
-
 	int a;
 	do {
 		scanf("%d", &a);
@@ -20,8 +19,8 @@ int main()
 }
 
 void pisiKljuc() {
-    Sleep(1000);        //koristi se da bi se smanjila mogucnost kreiranja istog kljuca
-    int i;
+	Sleep(1000);        //koristi se da bi se smanjila mogucnost kreiranja istog kljuca
+	int i;
 	FILE* f = fopen("kljuc.csv", "a+");
 	if (f == NULL) { printf("greska pri otvaranju datoteke!"); return; }
 	char kljuc[21];
@@ -32,8 +31,9 @@ void pisiKljuc() {
 		kljuc[i] = '0' + rand() % 10;
 	kljuc[4] = kljuc[9] = kljuc[14] = '-';
 	printf("Kljuc je kreiran: %s\n", kljuc);
-	if (!provjeri(kljuc, f)){
-        fprintf(f, "%s\n",kljuc);
+	if (!provjeri(kljuc, f)) {
+		int rb = index(f);
+		fprintf(f, "%d;%s\n",rb, kljuc);
 	}
 	else {
 		fclose(f);
@@ -44,14 +44,26 @@ void pisiKljuc() {
 
 int provjeri(char* kljuc, FILE* file) {
 	char niz[21];
-	while (fscanf(file, "%s", niz) != EOF) {
+	int rb;
+	while (fscanf(file, "%d;%s", &rb,niz) != EOF) {
 		if (!strcmp(kljuc, niz))
 			return 1;
 	}
 	return 0;
 }
 
-
+int index(FILE* f) {
+	int rb, i = 20;
+	char c;
+	if (ftell(f) == 0) return 1;		//ako nema kljuceva
+	while ((c = fgetc(f)) != '\n') {
+		fseek(f, -(i++), 2);
+		if (ftell(f) == 0) break;		//ako postoji samo jedan kljuc
+	}
+	fscanf(f,"%d", &rb);
+	fseek(f, 0, 2);
+	return rb+1;
+}
 
 
 
