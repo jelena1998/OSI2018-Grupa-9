b4bf481bba6919e19eb8f9070e40c969924dd9bc
Kovacevic24 <kovacevicbanjaluka98@gmail.com>
2019-01-08 16:11:00 +0100

    Uklonjene nekoristene funkcije

--- cetvrtaIgra/FunkcijeZaIspis.h
+++ cetvrtaIgra/FunkcijeZaIspis.h
@@ -1,20 +1,17 @@
 #ifndef FUNKCIJEZAISPIS_H_  // Ukljuci cuvara
 #define FUNKCIJEZAISPIS_H_
 
-void IspisTekstaSaPauzama(char*); // Ispisuje tekst sa pauzom tj. ispise dio teksta pa napravi pauzu
 void UsporeniIspisTeksta(int, char*); // Ispisuje karaktere stringa sa puzama
 void IspisiPrazanRed(int); // Ispisuje zadati broj praznih redova
-void IspisiKarakterVisePuta(char, int, int); // Ispisuje zadati karakter zadati broj puta u redu u zadatom broju redova
 void IspisGreskeONepravilnomUnosu(); // Ispisuje poruku o unosu nepostojece komande
 void SpecijalniIspis(char*); // Ispisuje <<<<<<<< tekst >>>>>>>>>>
 
 void IspisSlike(char*); // Ispisuje tekstualnu datoteku red po red
-void IspisPasosa(char*); // Prvo ispise jedan pasos tekstualne datoteke pa napravi pauzu pa onda opet nastavi
 void PisiZvijezde(); // Pomocna funkcija za ispis rezultata
 void PisiStrele(); // Pomocna funkcija za ispis rezultata
 void PisiRed(); // Pomocna funkcija za ispis rezultata
 void PisiLiniju(); // Pomocna funkcija za ispis rezultata
-void IspisRezultata(int, int, int); // Funkcija za formatirani ispis rezultata igre
+void IspisRezultata(int, int); // Funkcija za formatirani ispis rezultata igre
 
 void DaNe(char*); // Ispisuje DA i NE opcije HORIZONTALNO ili VERTIKALNO
 void DvaIzbora(char*, char*, char*); // Ispisuje dve zadate opcije HORIZONTALNO ili VERTIKALNO
